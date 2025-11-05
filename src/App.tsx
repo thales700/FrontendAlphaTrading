07aa64ef0d5ec93f41b14d2c475c49ff9407e17a
thalesuds700 <thales.uds@gmail.com>
2025-11-05 11:51:28 -0300

    Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React Alguém é gente boa de melhorar</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           contagem é {count}
